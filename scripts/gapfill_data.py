6b5c9c4195fc3c147f6543a9a39372e2ea912c68
Joseph Asplet <joseph.asplet@earth.ox.ac.uk>
2024-10-24 18:04:54 +0100

    changed gapfill to run on local machine for debugging

--- scripts/gapfill_data.py
+++ scripts/gapfill_data.py
@@ -1,133 +1,87 @@
 # Author: J Asplet, U of Oxford, 20/11/2023
 
 ## Python script to remotely query data from NYMAR array stations
-## This script is desgined to target explict (small) data gaps i.e, 1 or 2 channels in 1 day
-## Each set of channel, station, date must be given explicitly.
-# If download faitl for entire day, split into hour chunks and attempt to recombine at the end.
+## We are using Wget as implemented in the requests library
+## This script is designed to download data for specific channel
+## or network codes. For a bulk download from all insturments in a 
+## network or array, use download_data.py
+## In this script each tuple (net,sta,loc,chan, start, end)
+## must be provided. 
+## This script is intended to be used for trying to fillin gaps in 
+## your dataset. 
+
+## If the instruments are networked then you can downalod data from mutliple
+## Instruments by specifiying a dictionary of IP addresses. 
+## The example here uses a dictionary of IPs for intruments deployed for the North
+## York Moors Array (NYMAR) and is read in from a json file.
+
+## Data is requested in hourly chunks and then recombined into a day length miniSEED file
+
+## Some editing of this script could make it request minute chunks (for a whole day) or
+## make hourly / minutely requests for data 
 
 from pathlib import Path
 from obspy import UTCDateTime
-import obspy
 import timeit
 import datetime
-import requests
+import json
 import logging
+import itertools
+
+from data_pipeline import chunked_data_query, gather_chunks
+
 log = logging.getLogger(__name__)
+logdir = Path('/home/joseph/logs')
+# test is logdir exists, if not then set to cwd
+if logdir.exists():
+    print(f'Logs written to {logdir}')
+else:
+    logdir = Path.cwd()
+    print(f'Logs written to cwd')
 
-# functions
-
-def hour_by_hour_query(request, query_date):
-
-    hour_shift = datetime.timedelta(hours=1)
-    end = query_date + datetime.timedelta(days=1)
-    chunk_start = query_date 
-    chunk_end = query_date + hour_shift
-    while chunk_start < end:
-        query_start = chunk_start - 150
-        query_end = chunk_end + 150
-        year = chunk_start.year
-        month = chunk_start.month
-        day = chunk_start.day
-        hour = chunk_start.hour
-
-        ddir = Path(f'{path_cwd}/{year}/{month:02d}/{day:02d}')
-        outfile = Path(ddir, f"{request}.{year}{month:02d}{day:02d}T{hour:02d}0000_tmp.mseed")
-        if outfile.is_file():
-            log.info(f'Data chunk {outfile} exists')
-        else:
-            try:
-                make_request(station_ip, request, query_start, query_end)
-            except:
-                log.error(f'Unable to request hour {hour}')
-
-        chunk_start += day_shift
-        # Iterate otherwise we will have an infinite loop!
-
-def make_request(station_ip, request, start, end):
-
-    startUNIX = start.timestamp
-    endUNIX = end.timestamp
-    r = requests.get(f"http://{station_ip}:8080/data?channel={request}&from={startUNIX}&to={endUNIX}")
-    if r.status_code == 200:    
-        log.info(f'Request elapsed time {r.elapsed}')
-        if r.content:
-            with open(outfile, "wb") as f:
-                f.write(r.content)
-        else:
-            log.error('Request is empty! Wont write a zero byte file')
-    else:
-        log.erro(f'Request failed with status code: {r.status_code}')
-
-    return r
 
 if __name__ == '__main__':
-
     script_start = timeit.default_timer()
-    logging.basicConfig(filename='/home/joseph/logs/nymar_gapfill.log', level=logging.INFO)
+    logging.basicConfig(filename=f'{logdir}/nymar_backfill.log', level=logging.INFO)
     log.info(f'Starting download. Time is {datetime.datetime.now()}')
 
-    nym_zt_ips = {'NYM1':'172.24.59.19', 'NYM2':'172.24.239.162',
-                'NYM3':'172.24.40.146', 'NYM4':'172.24.77.181',
-                'NYM5':'172.24.43.200', 'NYM6':'172.24.150.216',
-                'NYM7':'172.24.194.185', 'NYM8':'172.24.3.251' }
 
     ######### Start of variable to set #############
+    # directory to write data to
+    # data_dir = Path('/home/joseph/data') # change to /your/path/to/datadir
+    data_dir = Path.cwd()
+    # Provide IP addresses. Here I have stored them in a JSON file to keep
+    # them off GitHub.
+    with open('/Users/eart0593/Projects/Agile/NYMAR/nymar_zerotier_ips.json','r') as w:
+        ips_dict = json.load(w)
+    
+    # Set up request parameters here. This is an example only. You may want
+    # To use this script as an exmample to build your own code which finds
+    # gaps that need filling and then sends the requests.
 
-    network = "OX"
-    station_list = ['NYM6']
-    channel_list = ['HHE'] 
-    #SET TO CORRECT CODE. should be '00' for veloctity data
-    # will be somehing different for voltage, check status page (https://{your-ip-here})
-    location = "00" 
-    # try to get previous 2 days of data (current day will not be available)
-    day_list = [UTCDateTime(2024, 6, 25, 0, 0, 0)]
+    request_params = [('OX','NYM2','00','HHN', UTCDateTime(2024, 10, 1, 0, 0, 0),UTCDateTime(2024,10,2, 0, 0, 0)),
+                      ('OX','NYM2','00','HHE', UTCDateTime(2024, 4, 28, 0, 0, 0),UTCDateTime(2024,4, 28, 0, 0, 0)),
+                      ('OX','NYM3','00','HHN', UTCDateTime(2023, 12, 1, 0, 0, 0),UTCDateTime(2023,12,2, 0, 0, 0)),
+                      ('OX','NYM4','00','HHZ', UTCDateTime(2024, 10, 1, 0, 0, 0),UTCDateTime(2024,10,2, 0, 0, 0))]
+    ########### End of variables to set ###########
 
-    if len(station_list) == len(channel_list) == len(day_list):
-
-        for i in range(0, len(station_list)):
-            log.info(f'Gap fill for: {station_list[i]}, {channel_list[i]}, {day_list[i]}.')
-            station = station_list[i] 
-            channel = channel_list[i]
-            query_date = day_list[i]
-            day_shift = datetime.timedelta(days=1)
-            station_ip = nym_zt_ips[station]
-            # path_cwd = Path('/Volumes/NYMAR_DATA/NYMAR/data_dump') / station
-            path_cwd = Path('/home/joseph/data')
-
-            # add a 2.5 minute buffer either side of date query to reduce gap risk
-            query_start = query_date - 150
-            query_end = query_date + day_shift + 150
-            startUNIX = query_start.timestamp
-            endUNIX = query_end.timestamp
-            year = query_date.year
-            month = query_date.month
-            day = query_date.day
-            hour = query_date.hour
-            ddir = Path(f'{path_cwd}/{year}/{month:02d}/{day:02d}')
-            ddir.mkdir(exist_ok=True, parents=True)
-
-            request = f"{network}.{station}.{location}.{channel}"
-            # Make filename to wirte out to
-            outfile = Path(ddir, f"{request}.{year}{month:02d}{day:02d}T{hour:02d}0000.mseed")
-            #Test if we have already downloaded this chunk
-            if outfile.is_file():
-                log.info(f'Data chunk {outfile} exists')
-            else:
-                timer_start = timeit.default_timer()
-                #time.sleep(5) # for testing directory creation
-                try:
-                        response = make_request(station_ip, request, query_start, query_end)
-                        
-                except Exception as e:
-                    log.exception(f'Handling Exception {e}. Try hourly chunks')
-                    try:
-                        hour_by_hour_query(request, query_date)
-                    except:
-                        log.error(f'Exception {e} could not be handled, move to next day')
-
-    # Get dataless (not really neccesary but its nice)
+    for params in request_params:
+        # params should be form (net, stat, loc, channel)
+        log.info(f'Request data for {params}')
+        station_ip = ips_dict[params[1]]
+    
+        chunked_data_query(station_ip, network=params[0], station=params[1],
+                           location=params[2], channel=params[3],
+                           starttime=params[4], endtime=params[5],
+                           data_dir=data_dir, chunksize=datetime.timedelta(hours=1),
+                           buffer=datetime.timedelta(seconds=150))
+        gather_chunks(network=params[0], station=params[1],
+                           location=params[2], channel=params[3],
+                           starttime=params[4], endtime=params[5],
+                           data_dir=data_dir, gather_size=datetime.timedelta(days=1))
 
     script_end = timeit.default_timer()
     runtime = script_end - script_start
 
     log.info(f'Runtime is {runtime:4.2f} seconds, or {runtime/60:4.2f} minutes, or {runtime/3600:4.2f} hours')
+
