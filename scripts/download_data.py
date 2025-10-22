9ac07a8eabc5e40b0206b560c714f0570b4b845e
Joseph Asplet <joseph.asplet@earth.ox.ac.uk>
2025-10-22 13:13:15 +0100

    typo fix

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -2,7 +2,7 @@
 
 # Python script to remotely query data from NYMAR array stations
 # We are using Wget as implemented in the requests library
-# This script is designed to download all available data 
+# This script is designed to download all available data
 # From an installed Certimus/Minimus for a given period of time.
 
 # If the instruments are networked then you can downalod data from mutliple
@@ -29,48 +29,48 @@ from obspy import UTCDateTime
 from data_pipeline import get_data
 
 log = logging.getLogger(__name__)
-logdir = Path('/home/joseph/logs')
+logdir = Path("/home/joseph/logs")
 # test is logdir exists, if not then set to cwd
 if logdir.exists():
-    print(f'Logs written to {logdir}')
+    print(f"Logs written to {logdir}")
 else:
     logdir = Path.cwd()
-    print(f'Logs written to cwd - {logdir}')
+    print(f"Logs written to cwd - {logdir}")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     script_start = timeit.default_timer()
-    logging.basicConfig(filename=f'{logdir}/nymar_backfill.log',
-                        level=logging.INFO)
-    log.info(f'Starting download. Time is {datetime.datetime.now()}')
+    logging.basicConfig(filename=f"{logdir}/nymar_backfill.log", level=logging.INFO)
+    log.info(f"Starting download. Time is {datetime.datetime.now()}")
 
     # ========== Start of variable to set ==========
     # directory to write data to
     # change to /your/path/to/datadir
-    data_dir = Path('/Users/eart0593/Projects/Agile/NYMAR/' +
-                    'data_dump/')
+    data_dir = Path("/Users/eart0593/Projects/Agile/NYMAR/" + "data_dump/")
     # Provide IP addresses. Here I have stored them in a JSON file to keep
     # them off GitHub.
-    with open('/Users/eart0593/Projects/Agile/NYMAR/nymar_zerotier_ips.json',
-              'r') as w:
+    with open("/Users/eart0593/Projects/Agile/NYMAR/nymar_zerotier_ips.json", "r") as w:
         ips_dict = json.load(w)
     # with open('/home/joseph/nymar_zerotier_ips.json', 'r') as w:
     #     ips_dict = json.load(w)
 
-    with open('/Users/eart0593/Projects/Agile/NYMAR/nymar_request_params.json', 'r') as param_file:
+    with open(
+        "/Users/eart0593/Projects/Agile/NYMAR/nymar_request_params.json", "r"
+    ) as param_file:
         params = json.load(param_file)
 
     # Seedlink Parameters
     networks = params["networks"]
     stations = params["stations"]
+    stations = ["NYM6"]
     channels = params["channels"]
     locations = params["locations"]
 
     # Time span to get data for. Edit these start/end objects
     # to customise the timespan to get data for.
-    start = [UTCDateTime(2025, 4, 3, 13, 0, 0)]
-    end = [UTCDateTime(2025, 4, 3, 15, 0, 0)]
-    log.info(f'Query start time: {start}')
-    log.info(f'Query end time: {end}')
+    start = [UTCDateTime(2024, 7, 16, 0, 0, 0)]
+    end = [UTCDateTime(2024, 8, 10, 0, 0, 0)]
+    log.info(f"Query start time: {start}")
+    log.info(f"Query end time: {end}")
     # SET TO CORRECT CODE. should be '00' for veloctity data
     # will be somehing different for voltage,
     # check Certimus/Minimus status page (https://{your-ip-here})
@@ -82,20 +82,17 @@ if __name__ == '__main__':
     # here start/end are the start and end time of all data to request
     # ========== End of variables to set ==========
 
-    request_params = itertools.product(networks,
-                                       stations,
-                                       locations,
-                                       channels,
-                                       start,
-                                       end)
+    request_params = itertools.product(
+        networks, stations, locations, channels, start, end
+    )
     # call get_data
-    asyncio.run(get_data(request_params,
-                         station_ips=ips_dict,
-                         data_dir=data_dir))
+    asyncio.run(get_data(request_params, station_ips=ips_dict, data_dir=data_dir))
 
     script_end = timeit.default_timer()
     runtime = script_end - script_start
 
-    log.info(f'Runtime is {runtime:4.2f} seconds,' +
-             f'or {runtime/60:4.2f} minutes,' +
-             f' or {runtime/3600:4.2f} hours')
+    log.info(
+        f"Runtime is {runtime:4.2f} seconds,"
+        + f"or {runtime/60:4.2f} minutes,"
+        + f" or {runtime/3600:4.2f} hours"
+    )
